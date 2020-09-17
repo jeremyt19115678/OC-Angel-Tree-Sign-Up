ea92ccb838f88160a6f9e66371dab8490736fa0b
Jeremy Tsai <jeremyt1114@g.ucla.edu>
2020-09-17 21:34:47 +0800

    Added force-refresh functionality.

--- Frontend/src/public/scripts/script.js
+++ Frontend/src/public/scripts/script.js
@@ -7,6 +7,16 @@ function initialize() {
     itemMasterList = [];
     itemViewList = [];
     fetchNewestLists();
+    // force reload code from StackOverflow
+    window.addEventListener( "pageshow", function ( event ) {
+        var historyTraversal = event.persisted || 
+                               ( typeof window.performance != "undefined" && 
+                                    window.performance.navigation.type === 2 );
+        if ( historyTraversal ) {
+          // Handle page restore.
+          window.location.reload();
+        }
+    });
 }
 
 //put the item count text and the table itself (@param) on the page
